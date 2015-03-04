5128f0b63be9b5787d8beab1b6e1363f5572e4b0
Chris Laughlin <bluntm@gmail.com>
2015-03-04 19:40:30 +0000

    getting and showing thumbs

--- js/views/app-view.js
+++ js/views/app-view.js
@@ -7,11 +7,27 @@ var app = app || {};
             'click #submitSubReddit': 'getThumbs'
         },
         initialize: function() {
+            this.thumbs = [];
             this.$grid = $('#photos');
             this.$subRedditInput = $('#subreddit');
         },
         getThumbs: function() {
-            alert(this.$subRedditInput.val());
+            this.$grid.html('');
+            $.get('https://www.reddit.com/r/'+this.$subRedditInput.val().trim()+'.json?limit=100')
+                .success(this.addThumbs.bind(this));
+        },
+        addThumbs: function(data) {
+            this.thumbs = Array.prototype.map.call(data.data.children, function(child) {
+                return new app.Thumb({
+                    name: child.data.title,
+                    thumbnail: child.data.thumbnail
+                });
+            });
+            this.thumbs.forEach(this.addOneThumb, this);
+        },
+        addOneThumb: function(thumb) {
+            var view = new app.ThumbView({ model: thumb});
+            this.$grid.append(view.render().el);
         }
     })
 })(jQuery);
--- js/views/thumb-view.js
+++ js/views/thumb-view.js
@@ -2,13 +2,17 @@ var app = app || {};
 (function ($) {
     'use strict';
     app.ThumbView = Backbone.View.extend({
-        tagName: 'img',
-        template: '',
+        tagName: 'div',
+        template: _.template($('#thumb-template').html()),
         events: {
             'click .thumbnail' : 'displayName'
         },
         displayName: function() {
             alert(this.model.get('name'));
+        },
+        render: function() {
+            this.$el.html(this.template(this.model.attributes));
+            return this;
         }
     })
 
